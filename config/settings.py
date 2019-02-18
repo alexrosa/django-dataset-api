6d6ec2a3739e5982b7cf09047aa259ceb619f977
Alexandre Rosa <alexrosa@gmail.com>
2019-02-17 21:38:37 -0300

    adding some changes

--- config/settings.py
+++ config/settings.py
@@ -125,9 +125,9 @@ DATE_INPUT_FORMATS = ['%Y-%m-%d HH:MM:SS']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/x/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'x'),
 )
 
 
